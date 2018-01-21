88a29aac7b34c642e09265a4a7034363f6ecadef
dvanmali <dvanmali@umail.ucsb.edu>
2018-01-21 03:14:33 -0800

    Purchasing methods added

--- javascript/home.js
+++ javascript/home.js
@@ -1,4 +1,20 @@
-function payMeButton() {
-    fastspring.builder.add('google-home');
+function addCheckoutItem(elmnt, item, color) {
+    // User wants to remove item
+    if (elmnt.style.borderColor == color) {
+        fastspring.builder.remove(item);
+        elmnt.style.borderColor = 'white';
+        elmnt.style.borderStyle = 'solid';
+        elmnt.style.borderWidth = '0px';
+    }
+    // User wants to add item
+    else {
+        fastspring.builder.add(item);
+        elmnt.style.borderColor = color;
+        elmnt.style.borderStyle = 'solid';
+        elmnt.style.borderWidth = '5px';
+    }
+}
+
+function checkout() {
     fastspring.builder.checkout();
 }
